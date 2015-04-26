6103fb0e7aa8453b19599f35f84ee2a98a33f23b
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2015-04-26 22:07:42 +0300

    Use Express static middleware to serve files

--- src/server/main.purs
+++ src/server/main.purs
@@ -2,6 +2,7 @@ module Main where
 
 import Debug.Trace
 import Data.Either
+import Data.Function
 import Data.Foreign.EasyFFI
 import Control.Monad.Eff
 import Control.Monad.Eff.Class
@@ -13,6 +14,9 @@ import qualified Node.FS.Async as File
 import qualified Node.Buffer as Buf
 import Node.Encoding
 
+foreign import staticMiddleware "var staticMiddleware = require('express').static"
+    :: String -> Fn3 Request Response (ExpressM Unit) (ExpressM Unit)
+
 logger :: Handler
 logger = do
     url <- getOriginalUrl
@@ -23,26 +27,12 @@ errorHandler :: Error -> Handler
 errorHandler err = do
     setStatus 400
     sendJson {error: message err}
-    
-fileHandler :: String -> Handler
-fileHandler file = do
-    cb <- capture sendContentsHandler
-    liftEff $ File.readFile file cb
-
-sendContentsHandler :: Either Error Buf.Buffer -> Handler
-sendContentsHandler eitherContents =
-    case eitherContents of
-        Left err ->
-            send $ show err
-        Right buffer ->
-            send $ Buf.toString UTF8 buffer
             
 appSetup :: App
 appSetup = do
     setProp "json spaces" 4
     use logger
-    get "/" $ fileHandler "public/index.html"
-    get "/public/js/client.js" $ fileHandler "public/js/client.js"
+    useExternal $ staticMiddleware "public"
     useOnError errorHandler
     
 main = do
